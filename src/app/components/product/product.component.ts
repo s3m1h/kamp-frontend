55b09990fe16f5f1ce01851b3ef20b37e9e5afc0
s3m1h <semih_acar01@hotmail.com>
2022-07-06 10:14:22 +0300

    19.ders ilk commit

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -14,7 +14,7 @@ export class ProductComponent implements OnInit {
 
   products:Product[] = [];
   dataLoaded = false;
-  
+  filterText="";  
   // HttpClient türünde bir nesne istiyorum diyoruz
   constructor(private productService:ProductService, private activatedRoute:ActivatedRoute) { }
 
